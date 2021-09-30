cecd8fa07927fc2633333925b1463751dcd896c2
Rhutesh <69714305+Rhutesh@users.noreply.github.com>
2021-09-30 11:11:19 +0530

    Error resolved

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
-using StarChart.Data.Models;
+using StarChart.Models;
 
-namespace StarChart.Properties.Controllers
+namespace StarChart.Controllers
 {
     [Route("")]
     [ApiController]
--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace StarChart.Data.Models
+namespace StarChart.Models
 {
     public class CelestialObject
     {
